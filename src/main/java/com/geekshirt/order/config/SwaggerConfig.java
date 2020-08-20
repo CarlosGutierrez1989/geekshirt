e417700f8e155a1a213cefe7764ec9104970b551
Carlos_Gutierrez <carlosgutierrez7233@gmail.com>
2020-08-20 21:43:51 +0200

    API Documentation done

--- src/main/java/com/geekshirt/order/config/SwaggerConfig.java
+++ src/main/java/com/geekshirt/order/config/SwaggerConfig.java
@@ -1,16 +1,27 @@
 package com.geekshirt.order.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+
+
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+
 public class SwaggerConfig {
+
 
     @Bean
     public Docket apiDocket(){
@@ -18,8 +29,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build().apiInfo(getApiInfo());
 
     }
-
+    private ApiInfo getApiInfo() {
+        return new ApiInfo( "GeekShirt Order Service API",
+                             "This API provides all methods required for order management",
+                              "1.0",
+                              "TERMS OF SERVICE URL",
+                            new Contact("Carlos Gutierrez",
+                                            "https://www.linkedin.com/in/cagv1103/",
+                                            "carlosgutierrez7233@gmail.com"),
+                                            "LICENSE",
+                                            "LICENSE-URL",
+                                             Collections.emptyList());
+    }
 }
--- src/main/java/com/geekshirt/order/dto/OrderRequest.java
+++ src/main/java/com/geekshirt/order/dto/OrderRequest.java
@@ -1,8 +1,23 @@
 package com.geekshirt.order.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+
 @Getter
+@ApiModel(description = "Class representing an order to be processed")
 public class OrderRequest {
+    @NotBlank
+    @NotNull
+    @ApiModelProperty(notes = "Account Id", example = "999999", required = true)
     String accountId;
+
+    @ApiModelProperty(notes = "Items included in the order", required = true )
+    private List<LineItem> items;
+
 }
